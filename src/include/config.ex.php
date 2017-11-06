5411f9eb99f159a6898f3b4be08bb30bd86842a7
Sean Kennedy <sean@kndy.net>
2017-11-06 14:26:59 -0700

    add example config

---
+++ src/include/config.ex.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+  'dbHost'      => 'localhost',
+  'dbUser'      => 'someuser',
+  'dbPassword'  => 'password',
+  'dbName'      => 'somedb',
+  'pollersDir'  => __DIR__ . '../../Pollers'
+];
